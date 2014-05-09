58afc160e2b0b9680c90d68ddcfdb7a180c69abc
David Zurborg <d.zurborg@propartner-it.de>
2014-05-09 14:55:35 +0200

    reordered documentation

--- lib/Dancer/Plugin/CRUD.pm
+++ lib/Dancer/Plugin/CRUD.pm
@@ -40,226 +40,6 @@ This plugin is derived from L<Dancer::Plugin::REST|Dancer::Plugin::REST> and hel
     name: "John Foo"
     email: "john.foo@example.com"
 
-=head1 METHODS
-
-=head2 C<< prepare_serializer_for_format >>
-
-When this pragma is used, a before filter is set by the plugin to automatically
-change the serializer when a format is detected in the URI.
-
-That means that each route you define with a B<:format> token will trigger a
-serializer definition, if the format is known.
-
-This lets you define all the REST actions you like as regular Dancer route
-handlers, without explicitly handling the outgoing data format.
-
-=head2 C<< resource >>
-
-This keyword lets you declare a resource your application will handle.
-
-Derived from L<Dancer::Plugin::REST|Dancer::Plugin::REST>, this method has rewritten to provide a more slightly convention. C<get> has been renamed to C<read> and three new actions has been added: C<index>, C<prefix> and C<prefix_id>
-
-Also, L<Text::Pluralize|Text::Pluralize> is applied to resource name with count=1 for singular variant and count=2 for plural variant. If you don't provide a singular/plural variant (i.e. resource name contains parenthesis) the singular and the plural becomes same.
-
-The id name is derived from singular resource name, appended with C<_id>.
-
-    resource 'user(s)' =>
-        index  => sub { ... }, # return all users
-        read   => sub { ... }, # return user where id = params->{user_id}
-        create => sub { ... }, # create a new user with params->{user}
-        delete => sub { ... }, # delete user where id = params->{user_id}
-        update => sub { ... }, # update user with params->{user}
-        prefix => sub {
-          # prefixed resource in plural
-          read '/foo' => sub { ... },
-        },
-        prefix_id => sub {
-          # prefixed resource in singular with id
-		  # params->{user_id}
-          read '/bar' => sub { ... },
-        };
-
-    # this defines the following routes:
-    # prefix_id =>
-    #   GET /user/:user_id/bar
-    # prefix =>
-    #   GET /users/foo
-    # read =>
-    #   GET /user/:id.:format
-    #   GET /user/:id
-    # index =>
-    #   GET /users.:format
-    #   GET /users
-    # create =>
-    #   POST /user.:format
-    #   POST /user
-    # delete =>
-    #   DELETE /user/:id.:format
-    #   DELETE /user/:id
-    # update =>
-    #   PUT /user/:id.:format
-    #   PUT /user/:id
-
-The routes are created in the above order.
-
-Hint: resources can be stacked with C<prefix>/C<prefix_id>:
-
-	resource foo =>
-		prefix => sub {
-			get '/bar' => sub {
-				return 'Hi!'
-			};
-		}, # GET /foo/bar
-		prefix_id => sub {
-			get '/bar' => sub {
-				return 'Hey '.param('foo_id')
-			}; # GET /foo/123/bar
-			resource bar =>
-				read => sub {
-					return 'foo is '
-						. param('foo_id')
-						.' and bar is '
-						. param('bar_id')
-					}
-				}; # GET /foo/123/bar/456
-		};
-
-When is return value is a HTTP status code (three digits), C<status(...)> is applied to it. A second return value may be the value to be returned to the client itself:
-
-	sub {
-		return 200
-	};
-	
-	sub {
-		return 404 => 'This object has not been found.'
-	}
-	
-	sub {
-		return 201 => { ... }
-	};
-	
-The default HTTP status code ("200 OK") differs in some actions: C<create> response with "201 Created", C<delete> and C<update> response with "202 Accepted".
-
-=head3 Change of suffix
-
-The appended suffix, C<_id> for default, can be changed by setting C<< $Dancer::Plugin::CRUD::SUFFIX >>. This affects both parameter names and the suffix of parameterized C<prefix> method:
-
-	$Dancer::Plugin::CRUD::SUFFIX = 'Id';
-	resource 'User' => prefixId => sub { return param('UserId') };
-
-=head2 helpers
-
-Some helpers are available. This helper will set an appropriate HTTP status for you.
-
-=head3 status_ok
-
-    status_ok({users => {...}});
-
-Set the HTTP status to 200
-
-=head3 status_created
-
-    status_created({users => {...}});
-
-Set the HTTP status to 201
-
-=head3 status_accepted
-
-    status_accepted({users => {...}});
-
-Set the HTTP status to 202
-
-=head3 status_bad_request
-
-    status_bad_request("user foo can't be found");
-
-Set the HTTP status to 400. This function as for argument a scalar that will be used under the key B<error>.
-
-=head3 status_not_found
-
-    status_not_found("users doesn't exists");
-
-Set the HTTP status to 404. This function as for argument a scalar that will be used under the key B<error>.
-
-=head1 LICENCE
-
-This module is released under the same terms as Perl itself.
-
-=head1 AUTHORS
-
-This module has been rewritten by David Zurborg C<< <zurborg@cpan.org> >>, based on code written by Alexis Sukrieh C<< <sukria@sukria.net> >> and Franck Cuny.
-
-=head1 SEE ALSO
-
-L<Dancer>
-L<http://en.wikipedia.org/wiki/Representational_State_Transfer>
-L<Dancer::Plugin::REST>
-L<Text::Pluralize>
-
-=head1 AUTHORS
-
-=over 4
-
-=item *
-
-David Zurborg <zurborg@cpan.org>
-
-=item *
-
-Alexis Sukrieh <sukria@sukria.net> (Author of Dancer::Plugin::REST)
-
-=item *
-
-Franck Cuny <franckc@cpan.org> (Author of Dancer::Plugin::REST)
-
-=back
-
-=head1 BUGS
-
-Please report any bugs or feature requests trough my project management tool
-at L<http://development.david-zurb.org/projects/libdancer-plugin-crud-perl/issues/new>. I
-will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Dancer::Plugin::CRUD
-
-You can also look for information at:
-
-=over 4
-
-=item * Redmine: Homepage of this module
-
-L<http://development.david-zurb.org/projects/libdancer-plugin-crud-perl>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Plugin-CRUD>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dancer-Plugin-CRUD>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/[Dancer-Plugin-CRUD>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Dancer-Plugin-CRUD/>
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2014 by David Zurborg <zurborg@cpan.org>.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
 =cut
 
 use Carp 'croak';
@@ -383,11 +163,20 @@ sub _generate_sub($) {
 	};
 }
 
-register send_entity => sub {
-    # entity, status_code
-    status($_[1] || 200);
-    $_[0];
-};
+=head1 METHODS
+
+=head2 C<< prepare_serializer_for_format >>
+
+When this pragma is used, a before filter is set by the plugin to automatically
+change the serializer when a format is detected in the URI.
+
+That means that each route you define with a B<:format> token will trigger a
+serializer definition, if the format is known.
+
+This lets you define all the REST actions you like as regular Dancer route
+handlers, without explicitly handling the outgoing data format.
+
+=cut
 
 register prepare_serializer_for_format => sub () {
 	my $conf        = plugin_setting;
@@ -424,6 +213,102 @@ register prepare_serializer_for_format => sub () {
         set(serializer => $default_serializer);
     });
 };
+
+=head2 C<< resource >>
+
+This keyword lets you declare a resource your application will handle.
+
+Derived from L<Dancer::Plugin::REST|Dancer::Plugin::REST>, this method has rewritten to provide a more slightly convention. C<get> has been renamed to C<read> and three new actions has been added: C<index>, C<prefix> and C<prefix_id>
+
+Also, L<Text::Pluralize|Text::Pluralize> is applied to resource name with count=1 for singular variant and count=2 for plural variant. If you don't provide a singular/plural variant (i.e. resource name contains parenthesis) the singular and the plural becomes same.
+
+The id name is derived from singular resource name, appended with C<_id>.
+
+    resource 'user(s)' =>
+        index  => sub { ... }, # return all users
+        read   => sub { ... }, # return user where id = params->{user_id}
+        create => sub { ... }, # create a new user with params->{user}
+        delete => sub { ... }, # delete user where id = params->{user_id}
+        update => sub { ... }, # update user with params->{user}
+        prefix => sub {
+          # prefixed resource in plural
+          read '/foo' => sub { ... },
+        },
+        prefix_id => sub {
+          # prefixed resource in singular with id
+		  # params->{user_id}
+          read '/bar' => sub { ... },
+        };
+
+    # this defines the following routes:
+    # prefix_id =>
+    #   GET /user/:user_id/bar
+    # prefix =>
+    #   GET /users/foo
+    # read =>
+    #   GET /user/:id.:format
+    #   GET /user/:id
+    # index =>
+    #   GET /users.:format
+    #   GET /users
+    # create =>
+    #   POST /user.:format
+    #   POST /user
+    # delete =>
+    #   DELETE /user/:id.:format
+    #   DELETE /user/:id
+    # update =>
+    #   PUT /user/:id.:format
+    #   PUT /user/:id
+
+The routes are created in the above order.
+
+Hint: resources can be stacked with C<prefix>/C<prefix_id>:
+
+	resource foo =>
+		prefix => sub {
+			get '/bar' => sub {
+				return 'Hi!'
+			};
+		}, # GET /foo/bar
+		prefix_id => sub {
+			get '/bar' => sub {
+				return 'Hey '.param('foo_id')
+			}; # GET /foo/123/bar
+			resource bar =>
+				read => sub {
+					return 'foo is '
+						. param('foo_id')
+						.' and bar is '
+						. param('bar_id')
+					}
+				}; # GET /foo/123/bar/456
+		};
+
+When is return value is a HTTP status code (three digits), C<status(...)> is applied to it. A second return value may be the value to be returned to the client itself:
+
+	sub {
+		return 200
+	};
+	
+	sub {
+		return 404 => 'This object has not been found.'
+	}
+	
+	sub {
+		return 201 => { ... }
+	};
+	
+The default HTTP status code ("200 OK") differs in some actions: C<create> response with "201 Created", C<delete> and C<update> response with "202 Accepted".
+
+=head3 Change of suffix
+
+The appended suffix, C<_id> for default, can be changed by setting C<< $Dancer::Plugin::CRUD::SUFFIX >>. This affects both parameter names and the suffix of parameterized C<prefix> method:
+
+	$Dancer::Plugin::CRUD::SUFFIX = 'Id';
+	resource 'User' => prefixId => sub { return param('UserId') };
+
+=cut
 
 register(resource => sub ($%) {
     my $resource = my $resource1 = my $resource2 = shift;
@@ -479,6 +364,48 @@ register(resource => sub ($%) {
     pop @respath;
 });
 
+=head2 helpers
+
+Some helpers are available. This helper will set an appropriate HTTP status for you.
+
+=head3 status_ok
+
+    status_ok({users => {...}});
+
+Set the HTTP status to 200
+
+=head3 status_created
+
+    status_created({users => {...}});
+
+Set the HTTP status to 201
+
+=head3 status_accepted
+
+    status_accepted({users => {...}});
+
+Set the HTTP status to 202
+
+=head3 status_bad_request
+
+    status_bad_request("user foo can't be found");
+
+Set the HTTP status to 400. This function as for argument a scalar that will be used under the key B<error>.
+
+=head3 status_not_found
+
+    status_not_found("users doesn't exists");
+
+Set the HTTP status to 404. This function as for argument a scalar that will be used under the key B<error>.
+
+=cut
+
+register send_entity => sub {
+    # entity, status_code
+    status($_[1] || 200);
+    $_[0];
+};
+
 for my $code (keys %http_codes) {
     my $helper_name = lc($http_codes{$code});
     $helper_name =~ s/[^\w]+/_/gms;
@@ -493,6 +420,87 @@ for my $code (keys %http_codes) {
         }
     };
 }
+
+=head1 LICENCE
+
+This module is released under the same terms as Perl itself.
+
+=head1 AUTHORS
+
+This module has been rewritten by David Zurborg C<< <zurborg@cpan.org> >>, based on code written by Alexis Sukrieh C<< <sukria@sukria.net> >> and Franck Cuny.
+
+=head1 SEE ALSO
+
+L<Dancer>
+L<http://en.wikipedia.org/wiki/Representational_State_Transfer>
+L<Dancer::Plugin::REST>
+L<Text::Pluralize>
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+David Zurborg <zurborg@cpan.org>
+
+=item *
+
+Alexis Sukrieh <sukria@sukria.net> (Author of Dancer::Plugin::REST)
+
+=item *
+
+Franck Cuny <franckc@cpan.org> (Author of Dancer::Plugin::REST)
+
+=back
+
+=head1 BUGS
+
+Please report any bugs or feature requests trough my project management tool
+at L<http://development.david-zurb.org/projects/libdancer-plugin-crud-perl/issues/new>. I
+will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::CRUD
+
+You can also look for information at:
+
+=over 4
+
+=item * Redmine: Homepage of this module
+
+L<http://development.david-zurb.org/projects/libdancer-plugin-crud-perl>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Plugin-CRUD>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer-Plugin-CRUD>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/[Dancer-Plugin-CRUD>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer-Plugin-CRUD/>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by David Zurborg <zurborg@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
 register_plugin;
 1;
